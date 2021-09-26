4807d9896bf825696c04e99ddd26cc3ebb73fe2a
hamzakussad <hamzakussad@hotmail.com>
2021-09-26 23:19:52 +0100

    menus class

---
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -0,0 +1,17 @@
+package uk.ac.ed.inf;
+
+import java.util.Scanner;
+
+public class Menus {
+    public String name;
+    public String port;
+    public Menus(String name, String port){
+        this.name = name;
+        this.port = port;
+    }
+
+
+    public int getDeliveryCost(String... strings) {
+        return 0;
+    }
+}
