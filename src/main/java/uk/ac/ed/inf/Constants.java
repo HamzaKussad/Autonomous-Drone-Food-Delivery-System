1d1c070ec26284e5cd33cde86731d5f3fffe596b
hamzakussad <hamzakussad@hotmail.com>
2021-09-30 19:31:22 +0100

    javadocs

--- src/main/java/uk/ac/ed/inf/Constants.java
+++ src/main/java/uk/ac/ed/inf/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final double LONG_MIN = -3.192473;
     public static final double MOVE_DIST = 0.00015;
     public static final int DELIVERY_COST = 50;
+    public static final int HOVERING_DRONE_VALUE = -999;
 }
--- src/main/java/uk/ac/ed/inf/LongLat.java
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -1,13 +1,28 @@
 package uk.ac.ed.inf;
 
+/**
+ * Class that represent a point on the map
+ * using Latitude and Longitude
+ */
 public class LongLat {
     public double longitude;
     public double latitude;
 
+    /**
+     * Creates a LongLat instance
+     * @param longitude longitude
+     * @param latitude latitude
+     */
     public LongLat(double longitude, double latitude){
         this.longitude = longitude;
         this.latitude = latitude;
     }
+
+    /**
+     * Checks if the point coordinates are within the confined area
+     * around main campus
+     * @return True if the point is in the confined area
+     */
 
     public boolean isConfined(){
         if((this.latitude >= Constants.LAT_MIN && this.latitude <= Constants.LAT_MAX) &&
@@ -17,9 +32,23 @@ public class LongLat {
         return false;
     }
 
+    /**
+     * gets the Pythagorean distance between the point
+     * and a different location
+     * @param location location of a different point coordinate
+     * @return the Pythagorean distance
+     */
+
     public double distanceTo(LongLat location) {
         return getPythagoreanDist(location);
     }
+
+    /**
+     * Checks if a location is (close to) within a move from the
+     * current location(point)
+     * @param location location of a different point coordinate
+     * @return True if the input location is close to the current point
+     */
 
     public boolean closeTo(LongLat location) {
         if (distanceTo(location) < Constants.MOVE_DIST){
@@ -28,13 +57,22 @@ public class LongLat {
         return false;
     }
 
-    public LongLat nextPosition(int i) {
+    /**
+     *
+     * @param i
+     * @return
+     */
+
+    public LongLat nextPosition(int facingAngle) throws Exception {
         LongLat nextMove;
-        if (i == -999){
+        if(facingAngle % 10 !=0){
+            throw new Exception("Angle must be multiple of 10");
+        }
+        if (facingAngle == Constants.HOVERING_DRONE_VALUE){
             nextMove = new LongLat(this.longitude, this.latitude);
         }else{
-            double longitude = this.longitude + (Math.cos(Math.toRadians(i)) * Constants.MOVE_DIST);
-            double latitude =  this.latitude + (Math.sin(Math.toRadians(i)) * Constants.MOVE_DIST);
+            double longitude = this.longitude + (Math.cos(Math.toRadians(facingAngle)) * Constants.MOVE_DIST);
+            double latitude =  this.latitude + (Math.sin(Math.toRadians(facingAngle)) * Constants.MOVE_DIST);
             System.out.println(longitude);
             System.out.println(latitude);
             nextMove = new LongLat(longitude, latitude);
